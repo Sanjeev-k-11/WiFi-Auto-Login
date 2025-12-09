c1e7267d7c86db8a8bc3e02be43558843e9d3bfc
sanjeev kumar yadav <148857871+Sanjeev-k-11@users.noreply.github.com>
2025-12-09 14:19:57 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,45 @@
-document.getElementById("save").addEventListener("click", () => {
-    const u = document.getElementById("user").value;
-    const p = document.getElementById("pass").value;
+const form = document.getElementById("form");
+const savedSection = document.getElementById("savedSection");
 
-    chrome.storage.local.set({ username: u, password: p }, () => {
-        alert("Saved successfully!");
+const userInput = document.getElementById("user");
+const passInput = document.getElementById("pass");
+const saveBtn   = document.getElementById("save");
+
+const updateBtn = document.getElementById("updateBtn");
+const removeBtn = document.getElementById("removeBtn");
+
+
+chrome.storage.local.get(["username","password"], (data)=>{
+
+    if(data.username){
+        form.style.display = "none";
+        savedSection.style.display = "block";
+    }
+});
+
+// save
+saveBtn.addEventListener("click", ()=>{
+    chrome.storage.local.set({
+        username: userInput.value,
+        password: passInput.value
+    },()=>{
+        form.style.display = "none";
+        savedSection.style.display = "block";
+    });
+});
+
+// update
+updateBtn.addEventListener("click", ()=>{
+    form.style.display = "block";
+    savedSection.style.display = "none";
+});
+
+// remove
+removeBtn.addEventListener("click", ()=>{
+    chrome.storage.local.remove(["username","password"], ()=>{
+        form.style.display = "block";
+        savedSection.style.display = "none";
+        userInput.value="";
+        passInput.value="";
     });
 });
